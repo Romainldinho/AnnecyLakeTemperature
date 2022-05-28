115a88aea9a39196cb57b432ab3e1dbbfe8ae974
Romain A <romain.archambault@gmail.com>
2022-05-29 01:29:08 +0200

    🦄🎨  Add conditional render

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,13 @@
 <div class="mx-2 mb-1 w-full max-w-80 mt-2">
-   <div class="px-1 bg-blue-300 text-black">Annecy Lake Temperature 🌡</div>
-      <p  class="px-1 bg-green-300 text-black mt-3">{{ $temp }}</p>
+   <div class="px-1 bg-white text-black">Annecy Lake Temperature 🌡</div>
+      @if ($temp >= 22)
+         <p  class="px-1 bg-green-300 text-black mt-3">{{ $temp }} °C 🥳</p>
+      @elseif ($temp >= 20)
+         <p  class="px-1 bg-green-200 text-black mt-3">{{ $temp }} °C 😎</p>
+      @elseif ($temp >= 18)
+         <p  class="px-1 bg-blue-300 text-black mt-3">{{ $temp }} °C 💪🏻</p>
+      @else
+         <p  class="px-1 bg-red-300 text-black mt-3">{{ $temp }} °C 🥶</p>
+      @endif
    </div>
 </div>
